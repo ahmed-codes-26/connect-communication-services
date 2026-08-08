042eef67557c88c2945341839906de3784610c76
Ahmed <ahmedjtt745@gmail.com>
2026-08-08 15:26:08 +0500

    Edited portfolio slider

--- app.js
+++ app.js
@@ -34,13 +34,16 @@ const swiper = new Swiper('.swiper', {
   },
   breakpoints: {
     0: {
-      slidesPerView: 1
+      slidesPerView: 1,
+      spaceBetween: 16
     },
     768: {
-      slidesPerView: 1
+      slidesPerView: 2,
+      spaceBetween: 24
     },
     1024: {
-      slidesPerView: 1
+      slidesPerView: 3,
+      spaceBetween: 30
     }
   }
 });
